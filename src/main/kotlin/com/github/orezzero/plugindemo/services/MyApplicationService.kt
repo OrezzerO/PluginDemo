5c29226710a5c29b7647ac2039cb0a30342cd726
GitHub Action <action@github.com>
2020-08-16 09:16:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/orezzero/plugindemo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/orezzero/plugindemo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.orezzero.plugindemo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.orezzero.plugindemo.MyBundle
 
 class MyApplicationService {
 
